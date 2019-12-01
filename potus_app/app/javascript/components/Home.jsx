55b399c3507a907dea904f475a2323f34011acb3
karoster <kosterb1@jhu.edu>
2019-12-01 13:22:38 -0500

    added frontend routing

---
+++ potus_app/app/javascript/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    Successfully configured frontend routes
+  </div>
+);
