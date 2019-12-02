e3d3b2add6ca46ce4eaab54e9a5af929bba96b07
karoster <kosterb1@jhu.edu>
2019-12-02 11:03:46 -0500

    added navbar

---
+++ potus_app/app/javascript/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class HeaderNav extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+    render(){
+
+        return(
+            <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+
+export default HeaderNav
--- potus_app/app/javascript/components/Home.jsx
+++ potus_app/app/javascript/components/Home.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import UnverifiedForm from "./UnverifiedForm"
+import HeaderNav from "./Header"
 import { Link } from "react-router-dom";
 
 export default () => (
   <div>
-    Successfully configured frontend routes
+    <HeaderNav/>
     <UnverifiedForm/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
   </div>
 );
