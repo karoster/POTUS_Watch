d497010c0a013b5e3d20862c90a95441a239b234
karoster <kosterb1@jhu.edu>
2019-12-03 18:40:48 -0500

    tweaked back end regex for validations

--- potus_app/app/controllers/api/v1/unverified_alerts_controller.rb
+++ potus_app/app/controllers/api/v1/unverified_alerts_controller.rb
@@ -2,14 +2,21 @@ class Api::V1::UnverifiedAlertsController < ApplicationController
 
 
     def create
-        #send email on creation success
         email = unverified_alert_params["email"]
         alert_words = unverified_alert_params["words"]
         @unverified_alert = UnverifiedAlert.where(email: email)
             .first_or_create(words: alert_words)
-            .update(words: alert_words)
-        if @unverified_alert 
-            render json: @unverified_alert
+
+
+        if @unverified_alert.valid?
+
+            if  @unverified_alert.update(words: alert_words)
+                render json: @unverified_alert
+            else
+                p @unverified_alert.errors.full_messages
+                render json: @unverified_alert.errors
+            end
+
         else
             render json: @unverified_alert.errors
         end
--- potus_app/app/models/unverified_alert.rb
+++ potus_app/app/models/unverified_alert.rb
@@ -4,22 +4,31 @@ class UnverifiedAlert < ApplicationRecord
     validate :valid_email_format, :valid_word_format 
     after_initialize :ensure_verification_token
 
+    # ensures email is properly formatted.
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    # only matches strings that have no whitespace. Frontend ensures words will 
+    # be formatted like this. Only mallicious requests can have whitespace.
+    VALID_WORD_REGEX = /^\S*$/i 
+
+
     def ensure_verification_token
         self.verification_token ||= SecureRandom.urlsafe_base64
     end
 
-    def vaild_word_format
+    def valid_word_format
         self.words.each do |word|
             if word.length > 20
                 errors.add(:words, "can't be longer than 20 characters")
-            elsif !word.match(/(?<=^[\s"']*)(\w+)/)
+            elsif !word.match(VALID_WORD_REGEX)
                 errors.add(:words, ": each keyword must only be a single word.")
             end
         end
+        errors.add(:words, "must not contain more than 5 keywords") if self.words.length > 5
+
     end
 
     def valid_email_format
-        if !self.email.match(/(?:[a-z0-9!/#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!/#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+        if !self.email.match(VALID_EMAIL_REGEX)
             errors.add(:email, "must be properly formatted")
         end
     end
