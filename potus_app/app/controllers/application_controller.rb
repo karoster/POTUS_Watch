8828519505458d2278375f7bae4f44c1f818a7b5
karoster <kosterb1@jhu.edu>
2019-12-09 15:34:49 -0500

    added custom api verification

--- potus_app/app/controllers/application_controller.rb
+++ potus_app/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    def verify_api_token
+        Rails.application.credentials.my_api[:auth_token] == params[:auth_token]
+        
+    end
+
+    def twitter_client
+        client = Twitter::REST::Client.new do |config|
+            config.consumer_key = Rails.application.credentials.twitter[:consumer_key]
+            config.consumer_secret = Rails.application.credentials.twitter[:consumer_key_secret]
+            config.access_token = Rails.application.credentials.twitter[:access_token]
+            config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
+        end
+    end
+
 end
