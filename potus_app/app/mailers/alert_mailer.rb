67269246c8724ff461db833ebca9eaf6878e575d
karoster <kosterb1@jhu.edu>
2019-12-09 15:34:24 -0500

    add alert email action

--- potus_app/app/mailers/alert_mailer.rb
+++ potus_app/app/mailers/alert_mailer.rb
@@ -3,6 +3,17 @@ class AlertMailer < ApplicationMailer
     def verify_alert_email(unverified_alert)
         @unverified_alert = unverified_alert
         mail(to: @unverified_alert.email, subject: "POTUS-Watch Alert Confirmation Email")
+
     end
 
+    def send_alert(alert, tweet_body, tweet_embed)
+        @tweet_embed = tweet_embed
+        @tweet_body = tweet_body
+        @alert_word, @alert_email = alert[1], alert[0]
+        mail(to: @alert_email, subject: "POTUS-Watch Notification Alert Email")
+    end
+
+    private
+
 end
+
