4f9452d46ac1fc5fa498f3bd3159f58b5553ba65
karoster <kosterb1@jhu.edu>
2019-12-09 15:33:35 -0500

    fixed bug where user could add more than 5 alerts

--- potus_app/app/controllers/api/v1/verified_alerts_controller.rb
+++ potus_app/app/controllers/api/v1/verified_alerts_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::VerifiedAlertsController < ApplicationController
   def show
     @unverified_alert = UnverifiedAlert.find_by(verification_token: verified_alert_params["id"])
-    p @unverified_alert
 
     if @unverified_alert
       begin
         VerifiedAlert.transaction do 
           words, email = @unverified_alert.words, @unverified_alert.email
+          VerifiedAlert.where(email: email).delete_all
+
           words.each { |word| tester = VerifiedAlert.create( email: email, word: word ) }
           @unverified_alert.destroy
         end
@@ -24,6 +25,8 @@ class Api::V1::VerifiedAlertsController < ApplicationController
 
   def destroy
     #implement later => remove from subscription list
+    #add verifiction token to verified table to delete alerts...
+    #prevents a user destroying another's records...
   end
 
   private
